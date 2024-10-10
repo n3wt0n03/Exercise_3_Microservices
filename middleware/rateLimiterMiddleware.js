88d38ddb0d8e16caac07ec8621480583177c8ca6
Marc Nelson B. Ochavo <89735506+n3wt0n03@users.noreply.github.com>
2024-10-10 23:52:43 +0800

    [FIX] fixed the maximum requests for rate limiter

--- middleware/rateLimiterMiddleware.js
+++ middleware/rateLimiterMiddleware.js
@@ -1,10 +1,10 @@
 const rateLimit = require('express-rate-limit');
 
 const apiRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 20, 
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // limit each requests to 10 per windowMs
   message: 'Too many requests, please try again after 15 minutes',
-  standardHeaders: true, 
+  standardHeaders: true,
   legacyHeaders: false,
 });
 
